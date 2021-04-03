827fa8857be0fe680379ddb9ae163db7d133be2d
Melnita Dabre <mdabre@umass.edu>
2021-04-03 16:36:40 -0400

    Parameterize main

--- threeinarow/src/RowGameApp.java
+++ threeinarow/src/RowGameApp.java
@@ -3,6 +3,8 @@ import controller.ThreeInARowController;
 import controller.TicTacToeController;
 import view.RowGameGUI;
 
+import java.util.Arrays;
+
 
 public class RowGameApp 
 {
@@ -10,9 +12,28 @@ public class RowGameApp
      * Starts a new game in the GUI.
      */
     public static void main(String[] args) {
-//	RowGameController game = new ThreeInARowController(4, 3);
-	RowGameController game = new TicTacToeController(4, 3);
-	RowGameGUI gui = new RowGameGUI(game);
-	game.resetGame();
+        if(args.length < 3){
+            throw new IllegalArgumentException("Missing arguments\n" +
+                    "Following arguments are needed : GameType Rows Columns\n" +
+                    "GameType: ThreeInARow, TicTacToe\n" +
+                    "e.g java RowGameApp ThreeInARow 3 3\n" +
+                    "e.g java RowGameApp TicTacToe 3 3");
+        }
+        int rows = Integer.parseInt(args[1]);
+        int cols = Integer.parseInt(args[2]);
+        RowGameController game;
+        String gameType = args[0];
+        switch(gameType){
+            case "ThreeInARow" : game = new ThreeInARowController(rows, cols);  break;
+            case "TicTacToe" : game = new TicTacToeController(rows, cols); break;
+            default: game = null;
+        }
+        if(game != null) {
+            RowGameGUI gui = new RowGameGUI(game);
+            game.resetGame();
+        }
+        else{
+            throw new IllegalArgumentException("Incorrect Game Type");
+        }
     }
 }
